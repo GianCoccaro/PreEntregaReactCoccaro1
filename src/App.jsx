d0effb04142d0570b6db957b7d68c7651c6d5bfc
Gianfranco Coccaro <coccarogianfranco@gmail.com>
2024-02-07 20:42:04 -0300

    Realizo CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,24 @@ import NavBar from './components/NavBar';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer';
+import { CartProvider } from './context/CartContext';
 
 function App() {
-
   return (
-    <>
+    <div className="App">
       <BrowserRouter>
-
-        <NavBar />
-
-        <Routes>
-          <Route exact path="/" element={ <ItemListContainer /> } />
-          <Route exact path="/category/:categoryId" element={ <ItemListContainer /> } />
-          <Route exact path="/item/:itemId" element={ <ItemDetailContainer /> } />
-          <Route exact path="*" element={<h1>404 NOT FOUND</h1>} />
-        </Routes>
+        <CartProvider>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={ <ItemListContainer greeting={"Todos los productos"} /> } />
+            <Route exact path="/category/:categoryId" element={ <ItemListContainer greeting={"Productos por categoría"} /> } />
+            <Route exact path="/item/:itemId" element={ <ItemDetailContainer /> } />
+            <Route exact path="*" element={<h1>404 NOT FOUND</h1>} />
+          </Routes>
+        </CartProvider>
       </BrowserRouter>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
